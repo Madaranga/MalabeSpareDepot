3570c44bf1cc689553f25dc986022f65f0219912
madaranga1351 <madaranga.20251351@iit.ac.lk>
2026-07-16 03:34:21 +0530

    design Checkout & Dealers tab and update MainController connections. Assigned unique fx:id attributes and linked

--- src/main/java/com/example/malabesparedepot/MainController.java
+++ src/main/java/com/example/malabesparedepot/MainController.java
@@ -2,19 +2,21 @@ package com.example.malabesparedepot;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController {
-
+    //TAB 1
     @FXML private TextField searchField;
     @FXML private ComboBox<String> categoryFilter;
     @FXML private Button btnSearch;
 
+    //TAB 2
     @FXML private TextField txtPartCode;
     @FXML private TextField txtName;
     @FXML private TextField txtBrand;
@@ -27,16 +29,33 @@ public class MainController {
     @FXML private Button btnUpdate;
     @FXML private Button btnDelete;
 
+    //TAB 3 - Left
+    @FXML private ComboBox<String> cmbCartPart;
+    @FXML private TextField txtCartQty;
+    @FXML private Button btnAddToCart;
+    //TAB 3 - Right
+    @FXML private TableView<?> tblCart;
+    @FXML private ComboBox<String> cmbDealer;
+    @FXML private Label lblTotal;
+    @FXML private Button btnCheckout;
+
+
+
+    //Initialization
     @FXML
     public void initialize() {
         System.out.println("Main Controller initialized successfully");
     }
 
+    //TAB 1 - Dashboard
     @FXML
     void onSearch() {
         System.out.println("Search Button clicked!");
     }
 
+
+
+    //TAB 2 - Manage Items
     @FXML
     void onAddPart() {
         System.out.println("Add Part Button clicked!");
@@ -50,6 +69,19 @@ public class MainController {
     @FXML
     void onDeletePart() {
         System.out.println("Delete Part Button clicked!");
+    }
+
+
+
+    //TAB 3 - Checkout & Dealers
+    @FXML
+    void onAddToCart() {
+        System.out.println("Add To Cart Button clicked!");
+    }
+
+    @FXML
+    void onCheckout() {
+        System.out.println("Process Checkout Button clicked!");
     }
 
 }

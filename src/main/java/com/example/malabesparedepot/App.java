ceb088ade6f318fd9c2188bf35a7091c6868c42d
madaranga1351 <madaranga.20251351@iit.ac.lk>
2026-07-16 01:32:16 +0530

    Implement App.java class to bootstrap and launch JavaFX application

--- src/main/java/com/example/malabesparedepot/App.java
+++ src/main/java/com/example/malabesparedepot/App.java
@@ -1,4 +1,37 @@
 package com.example.malabesparedepot;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            //Load fxml file
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/main_view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            //Set up window size
+            Scene scene = new Scene(root, 850, 600);
+
+            stage.setTitle("Malabe Spare Depot - Management System");
+            stage.setScene(scene);
+            stage.show();
+
+
+        } catch (IOException e) {
+            System.err.println("Error loading FXML file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

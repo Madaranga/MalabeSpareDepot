c93dbee4ecd3b50387e781fcd3874e7069c74465
madaranga1351 <madaranga.20251351@iit.ac.lk>
2026-07-15 22:32:35 +0530

    design main window TabPane and Inventory Dashboard layout in FXML

---
+++ src/main/java/com/example/malabesparedepot/MainController.java
@@ -0,0 +1,15 @@
+package com.example.malabesparedepot;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController {
+
+    @FXML
+    public void initialize() {
+        System.out.println("MainController initialized successfully");
+    }
+}

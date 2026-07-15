7815acaffab5515b244c8fc38387cac3872ee330
madaranga1351 <madaranga.20251351@iit.ac.lk>
2026-07-15 11:54:00 +0530

    PriceCalculator Added to handle bulk and discount rules

---
+++ src/main/java/com/example/malabesparedepot/model/PriceCalculator.java
@@ -0,0 +1,44 @@
+package com.example.malabesparedepot.model;
+
+import com.example.malabesparedepot.model.CartItem;
+import java.util.List;
+
+public class PriceCalculator {
+
+    public static double calculateFinalTotal(List<CartItem> cart) {
+        double runningTotal = 0.0;
+        boolean hasEngine = false;
+        boolean hasElectrical = false;
+
+        //Calculate item subtotals
+        for (CartItem item: cart) {
+            double itemSubtotal = item.getTotalprice();
+
+            if (item.getQuantity() >= 3) {
+                itemSubtotal = itemSubtotal * 0.95; // 5% discount
+            }
+            runningTotal += itemSubtotal;
+
+
+            //Track categories
+            String category = item.getPart().getCategory().toUpperCase();
+            if (category.contains("Electrical")) {
+                hasElectrical = true;
+            }
+            if (category.contains("Engine")) {
+                hasEngine = true;
+            }
+        }
+
+        //10% discount
+        if (hasEngine && hasElectrical) {
+            runningTotal = runningTotal*0.90;
+        }
+
+        return runningTotal;
+
+
+
+
+    }
+}
